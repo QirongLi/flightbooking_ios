750aa0f4fd1b261229454f4df9baf87ae56b9574
Qirong <Qirong@LiQirongs-MacBook-Pro.local>
2014-04-24 19:09:57 -0400

    Initial Commit

---
+++ flightbooking/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  flightbooking
+//
+//  Created by Qirong on 4/24/14.
+//  Copyright (c) 2014 LiQirong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
